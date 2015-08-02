6b1cbee0db03ed4e8b2a0841c6d265697ea60990
Matthew Rothenberg <mrothenberg@gmail.com>
2015-08-02 15:30:20 -0400

    Update scmpuff to v0.1.1

--- scmpuff.rb
+++ scmpuff.rb
@@ -1,7 +1,7 @@
 class Scmpuff < Formula
   homepage "https://mroth.github.io/scmpuff/"
-  url "https://github.com/mroth/scmpuff/archive/v0.1.0.tar.gz"
-  sha256 "2d276ee1c52ca5ef1f9572d89eef00fabc4431f8e43f2b9d53d9bc31ac0e0ec4"
+  url "https://github.com/mroth/scmpuff/archive/v0.1.1.tar.gz"
+  sha256 "cec3c9df41acb1735f2e8c1c9840d0481af0d996690f5a19a0a8fc4f06f97370"
 
   depends_on "go" => :build
 
@@ -17,11 +17,12 @@ class Scmpuff < Formula
     # do a compile, manually specifying version so that scmpuff's build script
     # does not do it's normal version detection which depends on being in a git
     # repository -- instead let homebrew recipe specify the version number.
-    system "GOPATH=#{gopath} go build -o bin/scmpuff -ldflags '-X main.VERSION #{version}'"
+    ENV["GOPATH"] = gopath
+    system "go build -o bin/scmpuff -ldflags '-X main.VERSION #{version}'"
     bin.install "bin/scmpuff"
   end
 
   test do
-    assert_equal "abc", shell_output("export e1=abc && #{bin}/scmpuff expand 1").strip
+    assert_equal "abc", shell_output("e1=abc #{bin}/scmpuff expand 1").strip
   end
 end
