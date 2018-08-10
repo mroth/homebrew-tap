2a54855bbaf07e6fea766e2ed6e46d22ae6f630c
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-10 17:48:09 -0400

    Brew formula update for deepclean version v0.1.0

---
+++ deepclean.rb
@@ -0,0 +1,11 @@
+class Deepclean < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/mroth/deepclean/releases/download/v0.1.0/deepclean_0.1.0_mac_x64.tar.gz"
+  version "0.1.0"
+  sha256 "ec3e3a4c0e4f345d703bc9493c21570489c984957f9b4a9b7daad6789534cfa1"
+
+  def install
+    bin.install "deepclean"
+  end
+end
