f32c15c08e9ee00622d454ec0763ace700646e8c
Matthew Rothenberg <mrothenberg@gmail.com>
2018-08-10 17:55:23 -0400

    Update deepclean.rb

--- deepclean.rb
+++ deepclean.rb
@@ -1,6 +1,6 @@
 class Deepclean < Formula
-  desc ""
-  homepage ""
+  desc "Scan and remove junk files from source code directories"
+  homepage "https://github.com/mroth/deepclean"
   url "https://github.com/mroth/deepclean/releases/download/v0.1.0/deepclean_0.1.0_mac_x64.tar.gz"
   version "0.1.0"
   sha256 "ec3e3a4c0e4f345d703bc9493c21570489c984957f9b4a9b7daad6789534cfa1"
